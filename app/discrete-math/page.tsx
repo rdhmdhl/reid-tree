1869ff4688d4fffc46fa4e8ba67f321a0debd850
rdhmdhl <reid@wearthefewture.com>
2024-06-13 09:11:22 -0700

    add content

---
+++ app/discrete-math/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link
+ from "next/link";
+
+export default function DiscreteMath() {
+  return (
+    <div className="flex flex-col justify-center font-mono font-bold text-center m-10">
+      <h2 className='text-2xl'>Discrete Math</h2>
+      <p className='text-l m-5'>What is Discrete Mathematics?</p>
+      <p className='text-sm font-thin text-left'>It's hard to explain. The counterpart to discrete math is continuous math, which can have a range of outcomes or results, whereas discrete math has distinct and separate values. Values in discreate math are countable and often integers. Continous math values are often real numbers, which can have an infinite amount of values within any interval.</p>
+      <p className='text-sm font-thin text-left mt-3'>Discrete math is the seed of many complex roots in computer science, such as opperating systems, algorithms, data structures, networking, database systems, and more.</p>
+      <div className="topics mt-10">
+        <Link href='discrete-math/set-theory'>
+          <p>Set Theory</p>
+        </Link>
+      </div>
+    </div>
+  )
+}
---
+++ app/discrete-math/set-theory/page.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+function SetTheory() {
+  return (
+    <div className="flex flex-col justify-center font-mono font-bold text-center m-10">
+        <h2 className='text-2xl'>Set Theory</h2>
+        <div className="members outline p-5 m-4 rounded">
+            <p className='text-l mb-4'>Members</p>
+            <p className='text-sm font-thin text-left'>Every item of a set is a "member". A set can also be a "member" of another set. The character &#8712; represents "is a member", while the character &#8713; represents "is not a member of". Set members are the unique values within the set itself. </p>
+            <p className='text-sm font-thin m-5'>{"s = {"} 0, 0, 1, 1, 0 {"}"}</p>
+            <p className='text-sm font-thin'>is the same as: </p>
+            <p className='text-sm font-thin mt-5'>{"s = {"} 0, 1 {"}"}</p>
+        </div>
+        <div className="set-builder-notation outline p-5 m-4 rounded">
+            <p className='text-l mb-4'>Set Builder Notation</p>
+            <p className='text-sm font-thin text-left'>The vertical bar in set building notation represents "such that". ℤ<sup>+</sup> below represents the set of positive integers. The notation below can be read as "x is a member of the positive integer set, and is less than 103".</p>
+            <p className='text-sm font-thin m-5'>{"x = {"} x | x satisfies one or more requirements {"}"}</p>
+            <p className='text-sm font-thin m-5'>{"s = {"} x | x&#8712;ℤ<sup>+</sup> and x {"<"} 103{"}"}</p>
+            <p className='text-sm font-thin m-5'> this can also be wrote as:</p>
+            <p className='text-sm font-thin m-5'>{"s = {"} x&#8712;ℤ<sup>+</sup> | x {"<"} 103{"}"}</p>
+        </div>
+        <div className="cardinality outline p-5 m-4 rounded">
+            <p className='text-l mb-4'>Cardinality</p>
+            <p className='text-sm font-thin m-5'>Number of distinct members of S</p>
+            <p className='text-sm font-thin m-5'>{"s = {"} 1, 2, 3, 3, 2, 1 {"}"}</p>
+            <p className='text-sm font-thin m-5'>|s| = 3</p>
+            <p className='text-sm font-thin m-5'>Finite sets have distinct members, while infite sets do not have a distinct number of member.</p>
+        </div>
+        <div className="more-properties outline p-5 m-4 rounded">
+            <p className='text-l mb-4'>More Set Properties</p>
+            <p className='text-sm font-thin m-5'>A and B are equal if </p>
+            <p className='text-sm font-thin m-5'>can be wrote as:</p>
+            <p className='text-sm font-thin m-5'>A = B if &#8704; x(x&#8712;A &#8660; x&#8712;B)</p>
+            <p className='text-sm font-thin m-5'>&#8704; represents "for all", and &#8660; represents "if and only if"</p>
+
+            <p className='text-sm font-thin m-5'>A is a subset of B if every element in A is also an element of B</p>
+            <p className='text-sm font-thin m-5'>this can be discribed as:</p>
+            <p className='text-sm font-thin m-5'>A &#8838; B</p>
+            <p className='text-sm font-thin m-5'>where &#8838; represents "is a subset or equal"</p>
+            <p className='text-sm font-thin m-5'>&#8840; would represent "is a subset and not equal"</p>
+        </div>
+    </div>
+  )
+}
+
+export default SetTheory
