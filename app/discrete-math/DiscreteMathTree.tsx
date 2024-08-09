c5c29e6a0a2777978575d1ba0376669482732cde
rdhmdhl <reid@wearthefewture.com>
2024-08-08 21:41:49 -0700

    add binary tree for discrete math

---
+++ app/discrete-math/DiscreteMathTree.tsx
@@ -0,0 +1,106 @@
+'use client';
+
+import React, { useEffect, useRef, MutableRefObject, forwardRef } from 'react';
+import dynamic from 'next/dynamic';
+import { NodeObject } from 'react-force-graph-2d';
+
+// Define your Node type that extends NodeObject
+interface MyNode extends NodeObject {
+    id: string;
+    name: string;
+  }
+
+const ForceGraph2D = dynamic(() => import('react-force-graph-2d'), { ssr: false });
+
+interface Node {
+  id: string;
+  name: string;
+}
+
+interface Link {
+  source: string;
+  target: string;
+}
+
+interface GraphData {
+  nodes: Node[];
+  links: Link[];
+}
+
+const ForwardedForceGraph2D = forwardRef<any, any>((props, ref) => (
+  <ForceGraph2D {...props} forwardRef={ref} />
+));
+
+
+const DiscreteMathTree: React.FC = () => {
+  const graphRef = useRef<any>();
+
+  useEffect(() => {
+    if (graphRef.current) {
+      const timeout = setTimeout(() => {
+        graphRef.current.zoomToFit(400);
+      }, 500);
+      return () => clearTimeout(timeout);
+    }
+  }, []);
+
+  const data: GraphData = {
+    nodes: [
+        { id: 'Root', name: 'Discrete Math' },
+        { id: 'LeftChild', name: 'Set Theory' },
+        { id: 'RightChild', name: 'Functions' },
+        { id: 'LeftGrandchild1', name: 'Graph Theory' },
+        { id: 'LeftGrandchild2', name: 'Combinatorics' },
+        { id: 'RightGrandchild1', name: 'Logic' },
+        { id: 'RightGrandchild2', name: 'Number Theory' }
+      ],
+      links: [
+        { source: 'Root', target: 'LeftChild' },
+        { source: 'Root', target: 'RightChild' },
+        { source: 'LeftChild', target: 'LeftGrandchild1' },
+        { source: 'LeftChild', target: 'LeftGrandchild2' },
+        { source: 'RightChild', target: 'RightGrandchild1' },
+        { source: 'RightChild', target: 'RightGrandchild2' }
+      ]
+  };
+
+  return (
+    <div className="flex justify-center items-center w-full">
+      <div className="justify-center">
+        <ForwardedForceGraph2D
+          ref={graphRef}
+          graphData={data}
+          nodeAutoColorBy="id"
+          nodeLabel="name"
+          d3VelocityDecay={0.9}
+          cooldownTicks={100}
+          enableNodeDrag={true}
+          enableZoomInteraction={true}
+          enablePanInteraction={true}
+          width={600}
+          height={600}
+          linkColor={() => 'white'}
+          linkDirectionalArrowLength={3.5}
+          linkDirectionalArrowRelPos={1}
+          onNodeClick={(node: MyNode) => {
+            // Handle click event, navigate to relevant link
+            const nodeId = node.id;
+            const links: { [key: string]: string } = {
+                'LeftChild': 'discrete-math/set-theory',
+                'RightChild': 'discrete-math/functions',
+                'LeftGrandchild1': 'discrete-math/graph-theory',
+                'LeftGrandchild2': 'discrete-math/combinatorics',
+                'RightGrandchild1': 'discrete-math/logic',
+                'RightGrandchild2': 'discrete-math/number-theory'
+              };
+            if (nodeId in links) {
+              window.location.href = links[nodeId];
+            }
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default DiscreteMathTree;
--- app/discrete-math/page.tsx
+++ app/discrete-math/page.tsx
@@ -1,54 +1,27 @@
-import React from 'react'
-import Link
- from "next/link";
+import React from "react";
+import DiscreteMathTree from "./DiscreteMathTree";
+import Link from "next/link";
 
 export default function DiscreteMath() {
   return (
     <div className="flex flex-col justify-center font-mono font-bold text-center m-10">
-      <h2 className='text-2xl'>Discrete Math</h2>
-      <p className='text-l m-5'>What is Discrete Mathematics?</p>
-      <p className='text-sm font-thin text-left'>It's hard to explain. The counterpart to discrete math is continuous math, which can have a range of outcomes or results, whereas discrete math has distinct and separate values. Values in discreate math are countable and often integers. Continous math values are often real numbers, which can have an infinite amount of values within any interval.</p>
-      <p className='text-sm font-thin text-left mt-3'>Discrete math is the seed of many complex roots in computer science, such as opperating systems, algorithms, data structures, networking, database systems, and more.</p>
-      <div className="flex flex-col items-center">
-        <div className="border-b-2 border-white h-8 mb-4"></div>
-        <div className="flex flex-col items-center space-y-8">
-          <div className="flex flex-col items-center border-2 border-gray-400 rounded p-4 cursor-pointer hover:bg-gray-700">
-            <Link href="discrete-math/set-theory" passHref>
-              <p className="text-xl mb-2">Set Theory</p>
-            </Link>
-          </div>
-          <div className="border-l-2 border-white h-8"></div>
-          <div className="flex flex-col items-center border-2 border-gray-400 rounded p-4 cursor-pointer hover:bg-gray-700">
-            <Link href="discrete-math/functions" passHref>
-              <p className="text-xl mb-2">Functions</p>
-            </Link>
-          </div>
-          <div className="border-l-2 border-white h-8"></div>
-          <div className="flex flex-col items-center border-2 border-gray-400 rounded p-4 cursor-pointer hover:bg-gray-700">
-            <Link href="discrete-math/graph-theory" passHref>
-              <p className="text-xl mb-2">Graph Theory</p>
-            </Link>
-          </div>
-          <div className="border-l-2 border-white h-8"></div>
-          <div className="flex flex-col items-center border-2 border-gray-400 rounded p-4 cursor-pointer hover:bg-gray-700">
-            <Link href="discrete-math/combinatorics" passHref>
-              <p className="text-xl mb-2">Combinatorics</p>
-            </Link>
-          </div>
-          <div className="border-l-2 border-white h-8"></div>
-          <div className="flex flex-col items-center border-2 border-gray-400 rounded p-4 cursor-pointer hover:bg-gray-700">
-            <Link href="discrete-math/logic" passHref>
-              <p className="text-xl mb-2">Logic</p>
-            </Link>
-          </div>
-          <div className="border-l-2 border-white h-8"></div>
-          <div className="flex flex-col items-center border-2 border-gray-400 rounded p-4 cursor-pointer hover:bg-gray-700">
-            <Link href="discrete-math/number-theory" passHref>
-              <p className="text-xl mb-2">Number Theory</p>
-            </Link>
-          </div>
-        </div>
-      </div>
+      <h2 className="text-2xl">Discrete Math</h2>
+      <p className="text-l m-5">What is Discrete Mathematics?</p>
+      <p className="text-sm font-thin text-left">
+        It's hard to explain. The counterpart to discrete math is continuous
+        math, which can have a range of outcomes or results, whereas discrete
+        math has distinct and separate values. Values in discreate math are
+        countable and often integers. Continous math values are often real
+        numbers, which can have an infinite amount of values within any
+        interval.
+      </p>
+      <p className="text-sm font-thin text-left mt-3">
+        Discrete math is the seed of many complex roots in computer science,
+        such as opperating systems, algorithms, data structures, networking,
+        database systems, and more.
+      </p>
+
+      <DiscreteMathTree />
     </div>
-  )
+  );
 }
